066b5760ad3478ca64b8561e314fd68a44015b96
mohdrafey1 <mohdrafey600@gmail.com>
2025-03-07 20:02:51 +0530

    liniting error fixed

--- src/redux/slices/collegesSlice.ts
+++ src/redux/slices/collegesSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from '../store';
 import { api } from '@/config/apiUrls';
 
