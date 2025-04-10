98fc8657a7060c3954ef25439bfdfbea352e52fc
mohdrafey1 <mohdrafey600@gmail.com>
2025-04-11 00:51:48 +0530

    bug fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -126,9 +126,7 @@ const Header = () => {
                     {isAuthenticated ? (
                         <div className='hidden md:flex items-center gap-3'>
                             <button
-                                onClick={() => {
-                                    handleSetting;
-                                }}
+                                onClick={handleSetting}
                                 className='p-2 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors'
                             >
                                 <Settings className='w-5 h-5 text-gray-600 dark:text-gray-400' />
